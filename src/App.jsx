90911b53ec7f482ac37aa6faf393d5fdc960d72e
xClune <aclune382@gmail.com>
2024-03-27 12:44:46 +0800

    More/Less description functionality in JobListing Component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Homecards from './components/Homecards'
 import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
 
 const App = () => {
   return (
@@ -11,14 +12,7 @@ const App = () => {
       <Hero />
       <Homecards />
       <JobListings />
-      
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-          >View All Jobs</a
-        >
-      </section>
+      <ViewAllJobs />
     </>
   )
 }
