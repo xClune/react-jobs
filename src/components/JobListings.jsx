e044445177944f84fc8d8398d6f79901b051ca48
xClune <aclune382@gmail.com>
2024-03-28 13:54:24 +0800

    Create mock back end for jobs.
    Fetch data in JobListings component with useEffect hook.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import jobs from '../jobs.json'
+import {React, useEffect, useState} from 'react'
 import JobListing from './JobListing'
 
 const JobListings = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  // fetch data from 'backend'
+  useEffect(() =>{
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs')
+        const data = await res.json();
+        setJobs(data);
+      } catch(error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchJobs();
+  }, []);
 
   return (
     <>
@@ -14,7 +30,7 @@ const JobListings = ({ isHome = false }) => {
             {isHome ? 'Recent Jobs' : 'Browse Jobs'}
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            { jobListings.map((job) => (
+            { jobs.map((job) => (
                 <JobListing key={ job.id } job={ job } />
             )) }
             </div>
