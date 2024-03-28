4430c32c003f764e33b760caa07d522a4ad0666c
xClune <aclune382@gmail.com>
2024-03-28 14:49:20 +0800

    Add spinner component on loading.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,5 +1,6 @@
 import {React, useEffect, useState} from 'react'
 import JobListing from './JobListing'
+import Spinner from './Spinner';
 
 const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
@@ -28,12 +29,15 @@ const JobListings = ({ isHome = false }) => {
         <div className="container-xl lg:container m-auto">
           <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
             {isHome ? 'Recent Jobs' : 'Browse Jobs'}
-          </h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            { jobs.map((job) => (
+          </h2>        
+            { loading 
+            ? <Spinner />
+            : <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                { jobs.map((job) => (
                 <JobListing key={ job.id } job={ job } />
             )) }
             </div>
+            }
         </div>
       </section>
     </>
