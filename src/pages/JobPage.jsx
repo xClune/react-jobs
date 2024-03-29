0c5b3b27c984a65fe905010371deacc8da202a6c
xClune <aclune382@gmail.com>
2024-03-29 16:15:13 +0800

    Add EditJob page and functionality (same form as AddJobPage).

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -5,10 +5,9 @@ import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 import { toast } from 'react-toastify'
 
 const JobPage = ({deleteJob}) => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const job = useLoaderData();
-
-  const navigate = useNavigate();
 
   const onDeleteClick = (jobId) => {
     const confirm = window.confirm('Are you sure?');
@@ -97,7 +96,7 @@ const JobPage = ({deleteJob}) => {
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to={`/jobs/edit/${job.id}`}
+                  to={`/edit-job/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                   >Edit Job</Link
                 >
